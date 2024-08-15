b4ee12508d7ab518bdce4a2c2fc9a8eac8ec05cc
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-08-15 12:31:49 +0530

    adding middlewares

---
+++ middlewares/auth_valid.js
@@ -0,0 +1,36 @@
+const { z } = require("zod");
+
+const signup_validation = (req, res, next) => {
+
+  const myschema = z.object({
+    username: z.string().min(1), // no need of using 'required'
+    email: z.tring().email({ message: "Invalid email address from zod du" }),
+    password: z.string().password(),
+  });
+  const { error } = myschema.safeParse(req.body);
+  if (error) {
+    return res.status(400).json({
+      msg: "badd request , invalid data",
+      error,
+    });
+  }
+  next();
+};
+
+const login_validation = (req, res, next) => {
+    
+  const myschema = z.object({
+    email: z.tring().email({ message: "Invalid email address from zod du" }),
+    password: z.string().password(),
+  });
+  const { error } = myschema.safeParse(req.body);
+  if (error) {
+    return res.status(400).json({
+      msg: "baddu request , invalid data",
+      error,
+    });
+  }
+  next();
+};
+
+module.exports = { signup_validation, login_validation };
--- routes/chatrouter.js
+++ routes/chatrouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { postingchat } = require("../controllers/chatcontroller");
 const { signup, login } = require("../controllers/signuplogin_controller");
+const { signup_validation, login_validation } = require("../middlewares/auth_valid");
 const p_router = express.Router();
 
 // creating routes
 // p_router.get("/ok", gettingchat);
-p_router.post("/signup", signup);
-p_router.post("/login", login);
+p_router.post("/signup", signup_validation, signup);
+p_router.post("/login", login_validation,login);
 p_router.post("/postchat", postingchat);
 
 module.exports = { p_router };
