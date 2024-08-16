52fb4fdee558f213bee04e956886107ca4d0fc8d
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-08-16 15:28:52 +0530

    changes are made in signuplogin_controller == === ()

--- controllers/signuplogin_controller.js
+++ controllers/signuplogin_controller.js
@@ -51,8 +51,8 @@ const login = async (req, res) => {
     }
 
     // const ispasswordequal = await bcrypt.compare(password, user.password);
-    ispasswordequal = (password == user.password);
-    if (ispasswordequal == false) {
+    ispasswordequal = password == user.password;
+    if (ispasswordequal === false) {
       return res.status(403).json({
         msg: "the email or password is wrong",
       });
